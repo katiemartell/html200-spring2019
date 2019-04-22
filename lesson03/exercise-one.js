096142c86a94febc04bccf3ba0be0309cec12338
Katie Martell <kaitlym.martell@gmail.com>
2019-04-21 21:13:00 -0700

    added lesson03 and exercise-one.js

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,8 @@
+let arr = ['sam', 'ezra', 'spot', 'button', 'fish'];
+console.log(arr);
+arr.push('sparky');
+console.log(arr);
+arr.splice(2, 1);
+console.log(arr);
+let string = arr.join(',');
+console.log(string);
